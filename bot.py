a816e9f9699f0f96195595ff5d23e8cf66f9b4d8
stephan <nanabotnax@gmail.com>
2026-09-11 12:22:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -209,7 +209,7 @@ def stream_status_loop():
         stream_online = True if TEST_MODE else is_stream_online()
 
         if stream_online and not stream_greeted:
-            sock.send(f"PRIVMSG {CHANNEL} :Здравствуйте, Нана! Хорошего стрима🌸 \r\n".encode())
+            sock.send(f"PRIVMSG {CHANNEL} :\r\n".encode())
             stream_greeted = True
 
         if not stream_online:
